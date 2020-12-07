969cbb0ff7be103918cc98677e638f4f62f5819b
DESKTOP-NBNVN40\User <gamzeguliter@gmail.com>
2020-12-07 13:33:49 +0300

    editor updates +jpeg (öykü buglara bakıcak)

--- code/src/sample/EditorScreen.java
+++ code/src/sample/EditorScreen.java
@@ -1,15 +1,22 @@
 package sample;
 
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class EditorScreen {
@@ -31,26 +38,29 @@ public class EditorScreen {
     Font font = Font.font("Source Sans Pro", 20);
 
     // constructors
-    public EditorScreen() {
+    public EditorScreen() throws FileNotFoundException {
         //editor = new Editor();
-        boardPane = getTiles(); //CHANGE
+        // boardPane = getTiles(); //CHANGE
         setScene();
     }
 
     // private methods
-    private void setScene() {
+    private void setScene() throws FileNotFoundException {
         Group group = new Group();
         int width = 1366;
         int height = 768;
 
-        group.getChildren().add(boardPane);
-
+        // group.getChildren().add(boardPane);
+        Rectangle[] recs = getTiles();
+        for(int i=0; i < 41; i++){
+            group.getChildren().add(recs[i]);
+        }
         scene = new Scene(group, width, height);
     }
 
-    private GridPane getTiles() {
-        GridPane gridPane = new GridPane();
-
+    private Rectangle[] getTiles() throws FileNotFoundException {
+        //  GridPane gridPane = new GridPane();
+        Rectangle[] recs = new Rectangle[41];
         for (int col = 0; col < 11; col++) {
             for (int row = 0; row < 11; row++) {
                 int sum = row + col;
@@ -67,43 +77,74 @@ public class EditorScreen {
                     pos = 40 + row + col;
 
                 if (pos < 40) {
-                    StackPane stp = new StackPane();
-                    stp.setPadding(new Insets(1, 1, 1, 1));
+                  /*  StackPane stp = new StackPane();
+                    stp.setPadding(new Insets(1, 1, 1, 1));*/
 
                     // create rectangle of correct color for tile
                     Rectangle tile = new Rectangle();
-                    if ((row == col) | (row == 0 & col == 10) | (col == 0 & row == 10)){
+                    if ( (row == 0 & col == 10) | (col == 0 & row == 10) | (row==10 & col == 10 ) | (row==0 & col == 0)){
                         tile.setHeight(60);
                         tile.setWidth(60);
+
+                        tile.setX(col* 60 + 10);
+                        tile.setY(row*60+10);
                     }
                     else if (row == 10 | row == 0){
                         tile.setHeight(60);
                         tile.setWidth(40);
+
+                        tile.setX(col* 40 + 10);
+                        tile.setY(row*60+10);
                     }
                     else{
                         tile.setHeight(40);
                         tile.setWidth(60);
+                        tile.setX(col* 60 + 10);
+                        tile.setY(row*40+10);
                     }
-                    tile.setX(col * 10);
-                    System.out.println(tile.getX());
-                    tile.setY(row * 10);
                     tile.setStroke(Color.BLACK);
                     tile.setFill(Color.WHITE);
                     tile.setOnMouseClicked(event -> {
                         System.out.println(pos);
                     });
 
-                    if ((row == 0) | (col == 0) | (row == 10) | (col == 10)) {
-                        stp.getChildren().add(0, tile);
+                    recs[pos]= tile;
+                  /*  if ((row == 0) | (col == 0) | (row == 10) | (col == 10)) {
+                        //stp.getChildren().add(0, tile);
                         //stp.getChildren().addAll(tile, text);
-                        gridPane.add(stp, col, row);
-                    }
+                        //gridPane.add(stp, col, row);
+                    }*/
                 }
             }
         }
-        gridPane.setLayoutX(10);
-        gridPane.setLayoutY(300);
-        return gridPane;
+        /*gridPane.setLayoutX(10);
+        gridPane.setLayoutY(300);*/
+        Rectangle middleOne = new Rectangle();
+        middleOne.setX(recs[0].getX()+60);
+        middleOne.setY(recs[0].getY()+60);
+        middleOne.setStroke(Color.BLACK);
+        middleOne.setFill(Color.WHITE);
+        middleOne.setHeight(360);
+        middleOne.setWidth(360);
+
+        Image image = new Image(new FileInputStream("C:\\Users\\User\\Documents\\cs319-Monopoly-3D\\code\\src\\sample\\ask.jpeg"));
+
+        //Setting the image view
+        //Setting the image view
+        ImagePattern imagePattern = new ImagePattern(image);
+
+        EventHandler<MouseEvent> eventHandler =
+                new EventHandler<javafx.scene.input.MouseEvent>() {
+
+                    @Override
+                    public void handle(javafx.scene.input.MouseEvent e) {
+                        middleOne.setFill(imagePattern);
+                    }
+                };
+//Adding the event handler
+        middleOne.addEventHandler(javafx.scene.input.MouseEvent.MOUSE_CLICKED, eventHandler);
+        recs[40]= middleOne;
+        return recs;
     }
 
     //new updateBoard same as getTiles?
@@ -146,9 +187,7 @@ public class EditorScreen {
                     tile.setX(col * 10);
                     tile.setY(row * 10);
                     tile.setStroke(Color.BLACK);
-                    tile.setOnMouseClicked(event -> {
-                        System.out.println(pos);
-                    });
+
 
                     if ((row == 0) | (col == 0) | (row == 10) | (col == 10)) {
                         stp.getChildren().add(0, tile);
