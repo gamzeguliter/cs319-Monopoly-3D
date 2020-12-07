a066f0ef2a9ce3886d32a08f98cf5f1f724d6b98
DESKTOP-NBNVN40\User <gamzeguliter@gmail.com>
2020-12-07 22:42:53 +0300

    not finished buggy version

--- code/src/sample/EditorScreen.java
+++ code/src/sample/EditorScreen.java
@@ -29,7 +29,7 @@ public class EditorScreen {
     Text[] playerTexts;
     Text turnText;
     Text diceText;
-
+GridPane recs;
     Button btnRollDice;
     Button btnEndTurn;
     Button btnBuy;
@@ -42,7 +42,7 @@ public class EditorScreen {
     // constructors
     public EditorScreen() throws FileNotFoundException {
         //editor = new Editor();
-        // boardPane = getTiles(); //CHANGE
+
         setScene();
     }
 
@@ -51,142 +51,15 @@ public class EditorScreen {
         Group group = new Group();
         int width = 1366;
         int height = 768;
+         recs = getTiles();
 
-        // group.getChildren().add(boardPane);
-        Rectangle[] recs = getTiles();
-        for(int i=0; i < 41; i++){
-            group.getChildren().add(recs[i]);
-        }
+
+        group.getChildren().add(recs);
         scene = new Scene(group, width, height);
     }
 
-    private Rectangle[] getTiles() throws FileNotFoundException {
-        //  GridPane gridPane = new GridPane();
-        Rectangle[] recs = new Rectangle[41];
-        for (int col = 0; col < 11; col++) {
-            for (int row = 0; row < 11; row++) {
-                int sum = row + col;
-                int pos;
-
-                if ( (row == 0) | (col == 0) | (row == 10) | (col == 10)) {
-                    if (col >= row) {
-                        pos = sum;
-                    } else {
-                        pos = 40 - sum;
-                    }
-                }
-                else
-                    pos = 40 + row + col;
-
-                if (pos < 40) {
-                  /*  StackPane stp = new StackPane();
-                    stp.setPadding(new Insets(1, 1, 1, 1));*/
-
-                    // create rectangle of correct color for tile
-                    Rectangle tile = new Rectangle();
-                    if ( (row == 0 & col == 10) | (col == 0 & row == 10) | (row == col)){
-                        tile.setHeight(90);
-                        tile.setWidth(90);
-                    }
-                    else if (row == 10 | row == 0){
-                        tile.setHeight(90);
-                        tile.setWidth(60);
-                    }
-                    else{
-                        tile.setHeight(60);
-                        tile.setWidth(90);
-                    }
-
-                    //rect locations
-                    if (pos == 0){
-                        tile.setX(10);
-                        tile.setY(10);
-                    }
-                    else if (pos <= 10){
-                        tile.setX(recs[pos - 1].getX() + recs[pos - 1].getWidth());
-                        tile.setY(recs[pos - 1].getY());
-                    }
-                    else if (pos <= 20){
-                        tile.setX(recs[pos - 1].getX());
-                        tile.setY(recs[pos - 1].getY() + recs[pos - 1].getHeight());
-                    }
-                    else if (pos < 30){
-                        tile.setX(recs[(pos + 1) % 40].getX() + recs[(pos + 1) % 40].getWidth());
-                        tile.setY(recs[(pos + 1) % 40].getY());
-                    }
-                    else if (pos < 40){
-                        tile.setX(recs[(pos + 1) % 40].getX());
-                        tile.setY(recs[(pos + 1) % 40].getY() + recs[(pos + 1) % 40].getHeight());
-                    }
-                    else{
-                        tile.setX(recs[0].getX() + recs[0].getHeight());
-                        tile.setY(recs[0].getY() + recs[0].getWidth());
-                    }
-
-                    //rect colors
-                    tile.setStroke(Color.BLACK);
-                    tile.setFill(Color.WHITE);
-                    tile.setOnMouseClicked(event -> {
-                        System.out.println(pos);
-                        Dialog d = new Dialog();
-                        d.setHeaderText("Select square type:");
-                        ComboBox squareType = new ComboBox();
-                        squareType.getItems().addAll(
-                                "Property",
-                                "Joker",
-                                "Chance",
-                                "Community Chest"
-                        );
-                        d.getDialogPane().getButtonTypes().addAll(ButtonType.CANCEL);
-                        d.show();
-                    });
-
-                    recs[pos]= tile;
-                    /*if ((row == 0) | (col == 0) | (row == 10) | (col == 10)) {
-                        stp.getChildren().add(0, tile);
-                        stp.getChildren().addAll(tile, text);
-                        gridPane.add(stp, col, row);
-                    }*/
-                }
-            }
-        }
-
-        /*gridPane.setLayoutX(10);
-        gridPane.setLayoutY(300);*/
-        Rectangle middleOne = new Rectangle();
-        middleOne.setX(recs[0].getX() + recs[0].getWidth());
-        middleOne.setY(recs[0].getY() + recs[0].getHeight());
-        middleOne.setStroke(Color.BLACK);
-        middleOne.setFill(Color.WHITE);
-        middleOne.setHeight(540);
-        middleOne.setWidth(540);
-
-        /* image yok
-        Image image = new Image(new FileInputStream("C:\\Users\\User\\Documents\\cs319-Monopoly-3D\\code\\src\\sample\\ask.jpeg"));
-
-        //Setting the image view
-        ImagePattern imagePattern = new ImagePattern(image);
-
-        EventHandler<MouseEvent> eventHandler =
-                new EventHandler<javafx.scene.input.MouseEvent>() {
-                    @Override
-                    public void handle(javafx.scene.input.MouseEvent e) {
-                        middleOne.setFill(imagePattern);
-                    }
-                };
-
-        //Adding the event handler
-        middleOne.addEventHandler(javafx.scene.input.MouseEvent.MOUSE_CLICKED, eventHandler);
-        recs[40]= middleOne;
-        return recs;
-    }
-    */
-
-
-    /*
-    //new updateBoard same as getTiles?
-    private void updateTiles() { //board pane vs grid pane?
-        boardPane.getChildren().clear();
+    private GridPane getTiles() {
+        GridPane gridPane = new GridPane();
 
         for (int col = 0; col < 11; col++) {
             for (int row = 0; row < 11; row++) {
@@ -224,22 +97,38 @@ public class EditorScreen {
                     tile.setX(col * 10);
                     tile.setY(row * 10);
                     tile.setStroke(Color.BLACK);
+                    //tile.setFill(Color.ORCHID);
+
+                    tile.setFill(Color.WHITE);
                     tile.setOnMouseClicked(event -> {
                         System.out.println(pos);
+                        Dialog d = new Dialog();
+                        d.setHeaderText("Select square type:");
+                        ComboBox squareType = new ComboBox();
+                        squareType.getItems().addAll(
+                                "Property",
+                                "Joker",
+                                "Chance",
+                                "Community Chest"
+                        );
+                        d.getDialogPane().getButtonTypes().addAll(ButtonType.CANCEL);
+                        d.show();
                     });
 
                     if ((row == 0) | (col == 0) | (row == 10) | (col == 10)) {
-                        stp.getChildren().add(0, tile);
-                        //stp.getChildren().addAll(tile, text);
+                        //stp.getChildren().add(0, tile);
+                        stp.getChildren().addAll(tile);
+                        gridPane.add(stp, col, row);
                     }
-                    boardPane.add(stp, col, row);
                 }
             }
-        }*/
-        //middleOne.addEventHandler(javafx.scene.input.MouseEvent.MOUSE_CLICKED, eventHandler);
-        recs[40]= middleOne;
-        return recs;
+        }
+        gridPane.setLayoutX(10);
+        gridPane.setLayoutY(300);
+        return gridPane;
     }
+
+
 
 
     // public methods
