33b1630d11274730a82c92097fc069381db12ef2
LAPTOP-2OIG33ED\User <melisataspinar@windowslive.com>
2020-12-20 19:11:56 +0300

    Help and Credits Screens Implemented

---
+++ code/src/sample/screens/CreditsScreen.java
@@ -0,0 +1,39 @@
+package sample.screens;
+
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import sample.ScreenManager;
+
+
+import java.io.IOException;
+
+public class CreditsScreen extends Screen {
+
+    private Label infoLabel;
+    AnchorPane creditsScreen = FXMLLoader.load(getClass().getResource("../layouts/MainMenuScreen.fxml"));
+
+    public CreditsScreen(ScreenManager screenManager) throws IOException {
+        super(screenManager);
+        infoLabel = new Label("CREDITS\nCansu Moran\nElif Gamze Güliter\nMelisa Taşpınar\nÖykü Irmak Hatipoğlu\nYiğit Gürses");
+        initializeScene();
+    }
+
+    private void initializeScene() {
+        VBox vbox = (VBox) creditsScreen.getChildren().get(1);
+        infoLabel.setAlignment(Pos.CENTER);
+        vbox.getChildren().addAll(infoLabel);
+        vbox.setAlignment(Pos.CENTER);
+        scene = new Scene(creditsScreen);
+    }
+
+    @Override
+    public Scene getScene() {
+        return scene;
+    }
+}
